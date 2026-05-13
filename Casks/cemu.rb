964aae170962a0338fb6e0862ac823120a756ba7
Ousmane Kane <o.k@etherbox.me>
2026-05-13 11:16:05 +0200

    Add Cemu cask for Wii U emulator

---
+++ Casks/cemu.rb
@@ -0,0 +1,18 @@
+cask "cemu" do
+  version "2.6"
+  sha256 "698c4b298f94983e4d6c30e9687ba8ff05094dd3930837c5104cddc0b0a49e4e"
+
+  url "https://github.com/cemu-project/Cemu/releases/download/v#{version}/cemu-#{version}-macos-12-x64.dmg",
+      verified: "github.com/cemu-project/Cemu/"
+
+  name "Cemu"
+  desc "Wii U emulator"
+  homepage "https://cemu.info/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Cemu.app"
+end
